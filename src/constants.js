b83d547f526b2ab0c72702a1a9a2c25a22fbcd19
veggie-garden <devcseo@gmail.com>
2021-11-28 02:28:45 +0900

    refactor: change export of constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,19 @@
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 9;
 const MAX_LENGTH = 3;
-export { MIN_NUMBER, MAX_NUMBER, MAX_LENGTH };
 
-const USER_INPUT_NUMBER_DUPLICATE = "중복되는 숫자 없이 입력해주세요.";
-const USER_INPUT_NUMBER_OVERLENGTH = "세 자릿수 숫자를 입력해주세요.";
-export { USER_INPUT_NUMBER_DUPLICATE, USER_INPUT_NUMBER_OVERLENGTH };
+const USER_INPUT_NUMBER_DUPLICATED = "중복되는 숫자 없이 입력해주세요.";
+const USER_INPUT_NUMBER_LENGTH_NOT_EXCEED = "세 자릿수 숫자를 입력해주세요.";
 
 const CORRECT_ANSWER = "정답을 맞혔습니다!";
 const ASK_RESTART = "게임을 새로 시작하겠습니까?";
-export { CORRECT_ANSWER, ASK_RESTART };
+
+export {
+  MIN_NUMBER,
+  MAX_NUMBER,
+  MAX_LENGTH,
+  USER_INPUT_NUMBER_DUPLICATED,
+  USER_INPUT_NUMBER_LENGTH_NOT_EXCEED,
+  CORRECT_ANSWER,
+  ASK_RESTART,
+};
