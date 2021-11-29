a9e2f8e312be6bf7754b2c74e810de3d5b0c41ea
veggie-garden <devcseo@gmail.com>
2021-11-29 18:56:07 +0900

    feat: add function that checks zero included or not

--- src/check_valid.js
+++ src/check_valid.js
@@ -1,11 +1,16 @@
 import {
   MAX_LENGTH,
+  USER_INPUT_INCLUDE_ZERO,
   USER_INPUT_NUMBER_DUPLICATED,
   USER_INPUT_NUMBER_LENGTH_NOT_EXCEED,
 } from "./constants.js";
 
 const isNotExceedCondition = (userInput) => {
   return userInput.length !== MAX_LENGTH;
+};
+
+const isZeroInclude = (userInput) => {
+  return userInput.includes(0);
 };
 
 const isDuplicatedUserInput = (userInput) => {
@@ -19,6 +24,10 @@ export default function isCheckValidUserInput(userInput) {
   }
   if (isDuplicatedUserInput(userInput)) {
     alert(USER_INPUT_NUMBER_DUPLICATED);
+    return false;
+  }
+  if (isZeroInclude(userInput)) {
+    alert(USER_INPUT_INCLUDE_ZERO);
     return false;
   }
   return true;
