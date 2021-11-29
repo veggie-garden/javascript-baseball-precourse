91ec2f9b52e6f2ae2ed5f1393cecb91e8ea8fc76
veggie-garden <devcseo@gmail.com>
2021-11-29 22:00:16 +0900

    refactor: export function

--- src/index.js
+++ src/index.js
@@ -8,94 +8,94 @@ import {
   NO_CORRECT_ANSWER,
 } from "./constants.js";
 
-export default function BaseballGame() {
-  const randomComputerInputNumber = () => {
-    const flag = new Array(10).fill(0);
-    let computerInputNumber = "";
-    while (computerInputNumber.length < MAX_LENGTH) {
-      let num = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-      while (flag[num] === 1) {
-        num = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-      }
-      flag[num] = 1;
-      computerInputNumber += num;
+const randomComputerInputNumber = () => {
+  const flag = new Array(10).fill(0);
+  let computerInputNumber = "";
+  while (computerInputNumber.length < MAX_LENGTH) {
+    let num = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    while (flag[num] === 1) {
+      num = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
-    return computerInputNumber;
-  };
+    flag[num] = 1;
+    computerInputNumber += num;
+  }
+  return computerInputNumber;
+};
 
-  const saveRandomComputerInputNumber = () => {
-    const computerNumber = randomComputerInputNumber();
-    localStorage.setItem("computerInputNumber", computerNumber);
-  };
+const saveRandomComputerInputNumber = () => {
+  const computerNumber = randomComputerInputNumber();
+  localStorage.setItem("computerInputNumber", computerNumber);
+};
 
-  const getRandomComputerInputNumber = () => {
-    return localStorage.getItem("computerInputNumber");
-  };
+const getRandomComputerInputNumber = () => {
+  return localStorage.getItem("computerInputNumber");
+};
 
-  const compareInputs = (computerInputNumbers, userInputNumbers) => {
-    let [ball, strike] = [0, 0];
-    for (let i = 0; i < MAX_LENGTH; i += 1) {
-      if (computerInputNumbers[i] === userInputNumbers[i]) {
-        strike += 1;
-      } else if (userInputNumbers.includes(computerInputNumbers[i])) {
-        ball += 1;
-      }
+const compareInputs = (computerInputNumbers, userInputNumbers) => {
+  let [ball, strike] = [0, 0];
+  for (let i = 0; i < MAX_LENGTH; i += 1) {
+    if (computerInputNumbers[i] === userInputNumbers[i]) {
+      strike += 1;
+    } else if (userInputNumbers.includes(computerInputNumbers[i])) {
+      ball += 1;
     }
-    return [ball, strike];
-  };
+  }
+  return [ball, strike];
+};
 
-  const clean = () => {
-    const userInput = document.getElementById("user-input");
-    userInput.value = "";
-    const result = document.getElementById("result");
-    result.textContent = "";
-  };
+const clean = () => {
+  const userInput = document.getElementById("user-input");
+  userInput.value = "";
+  const result = document.getElementById("result");
+  result.textContent = "";
+};
 
-  const restart = () => {
-    saveRandomComputerInputNumber();
-    clean();
-  };
+const restart = () => {
+  saveRandomComputerInputNumber();
+  clean();
+};
 
-  const printResult = ([ball, strike]) => {
-    const result = document.getElementById("result");
-    if (ball === 0 && strike === 0) {
-      result.innerText = NO_CORRECT_ANSWER;
-    } else if (strike === 3) {
-      result.innerHTML = CORRECT_ANSWER + ASK_RESTART;
-      const restartButton = document.getElementById("game-restart-button");
-      restartButton.onclick = restart;
-    } else if (ball === 0) {
-      result.innerText = `${strike}스트라이크`;
-    } else if (strike === 0) {
-      result.innerText = `${ball}볼`;
+const printResult = ([ball, strike]) => {
+  const result = document.getElementById("result");
+  if (ball === 0 && strike === 0) {
+    result.innerText = NO_CORRECT_ANSWER;
+  } else if (strike === 3) {
+    result.innerHTML = CORRECT_ANSWER + ASK_RESTART;
+    const restartButton = document.getElementById("game-restart-button");
+    restartButton.onclick = restart;
+  } else if (ball === 0) {
+    result.innerText = `${strike}스트라이크`;
+  } else if (strike === 0) {
+    result.innerText = `${ball}볼`;
+  } else {
+    result.innerText = `${ball}볼 ${strike}스트라이크`;
+  }
+};
+
+const play = (computerInputNumbers, userInputNumbers) => {
+  printResult(compareInputs(computerInputNumbers, userInputNumbers));
+};
+
+const getUserInput = () => {
+  const userInputButton = document.getElementById("submit");
+  const userInput = document.getElementById("user-input");
+  userInputButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!isCheckValidUserInput(userInput.value)) {
+      userInput.value = "";
     } else {
-      result.innerText = `${ball}볼 ${strike}스트라이크`;
+      play(getRandomComputerInputNumber(), userInput.value);
     }
-  };
+  });
+};
 
-  const play = (computerInputNumbers, userInputNumbers) => {
-    printResult(compareInputs(computerInputNumbers, userInputNumbers));
-  };
-
-  const getUserInput = () => {
-    const userInputButton = document.getElementById("submit");
-    const userInput = document.getElementById("user-input");
-    userInputButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (!isCheckValidUserInput(userInput.value)) {
-        userInput.value = "";
-      } else {
-        play(getRandomComputerInputNumber(), userInput.value);
-      }
-    });
-  };
-
-  const init = () => {
+export default function BaseballGame() {
+  this.init = () => {
     saveRandomComputerInputNumber();
     getUserInput();
   };
 
-  init();
+  this.init();
 }
 
-BaseballGame();
+new BaseballGame();
