8733e94b247ad1e868c59c464b9cf57e582559e8
veggie-garden <devcseo@gmail.com>
2021-11-29 20:52:16 +0900

    chore: add and change constants

--- src/constants.js
+++ src/constants.js
@@ -6,8 +6,11 @@ const USER_INPUT_INCLUDE_ZERO = "0은 사용할 수 없습니다.";
 const USER_INPUT_NUMBER_DUPLICATED = "중복되는 숫자 없이 입력해주세요.";
 const USER_INPUT_NUMBER_LENGTH_NOT_EXCEED = "세 자릿수 숫자를 입력해주세요.";
 
-const CORRECT_ANSWER = "정답을 맞혔습니다!";
-const ASK_RESTART = "게임을 새로 시작하겠습니까?";
+const NO_CORRECT_ANSWER = "낫싱";
+const CORRECT_ANSWER =
+  "<p><span>🎉</span> <strong>정답을 맞추셨습니다!</strong> <span>🎉</span></p>";
+const ASK_RESTART =
+  "<p><span>게임을 새로 시작하겠습니까?</span> <button id='game-restart-button'>게임 재시작</button></p>";
 
 export {
   MIN_NUMBER,
@@ -16,6 +19,7 @@ export {
   USER_INPUT_INCLUDE_ZERO,
   USER_INPUT_NUMBER_DUPLICATED,
   USER_INPUT_NUMBER_LENGTH_NOT_EXCEED,
+  NO_CORRECT_ANSWER,
   CORRECT_ANSWER,
   ASK_RESTART,
 };
