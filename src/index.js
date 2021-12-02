53942eb4e6305f8eb77c2de2ddb72225bbf701c1
veggie-garden <devcseo@gmail.com>
2021-12-02 19:58:11 +0900

    refactor: return the value when localStorage has random number key else run generateRandomNumber(), change restartButton.onclick to addEventListener in printResult()

--- src/index.js
+++ src/index.js
@@ -27,11 +27,18 @@ const saveRandomComputerInputNumber = () => {
 };
 
 const getRandomComputerInputNumber = () => {
-  return localStorage.getItem(LOCALSTORAGE_RANDOM_NUMBER_KEY);
+  const computerNumber = localStorage.getItem(LOCALSTORAGE_RANDOM_NUMBER_KEY);
+
+  if (computerNumber) {
+    return computerNumber;
+  }
+
+  return saveRandomComputerInputNumber();
 };
 
 const compareInputs = (computerInputNumbers, userInputNumbers) => {
   let [ball, strike] = [0, 0];
+
   for (let i = 0; i < NUMS.MAX_LENGTH; i += 1) {
     if (computerInputNumbers[i] === userInputNumbers[i]) {
       strike += 1;
@@ -39,13 +46,14 @@ const compareInputs = (computerInputNumbers, userInputNumbers) => {
       ball += 1;
     }
   }
+
   return [ball, strike];
 };
 
 const clean = () => {
   const userInput = document.getElementById("user-input");
-  userInput.value = "";
   const result = document.getElementById("result");
+  userInput.value = "";
   result.textContent = "";
 };
 
@@ -56,17 +64,18 @@ const restart = () => {
 
 const printResult = ([ball, strike]) => {
   const result = document.getElementById("result");
+
   if (ball === 0 && strike === 0) {
     result.innerText = RESULT_REPLY.NO_CORRECT_ANSWER;
   } else if (strike === 3) {
     result.innerHTML = RESULT_REPLY.CORRECT_ANSWER + RESULT_REPLY.ASK_RESTART;
     const restartButton = document.getElementById("game-restart-button");
-    restartButton.onclick = restart;
+    restartButton.addEventListener("click", restart());
   } else if (ball === 0) {
     result.innerText = `${strike}스트라이크`;
   } else if (strike === 0) {
     result.innerText = `${ball}볼`;
-  } else {
+  } else if (ball && strike) {
     result.innerText = `${ball}볼 ${strike}스트라이크`;
   }
 };
@@ -78,6 +87,7 @@ const play = (computerInputNumbers, userInputNumbers) => {
 const getUserInput = () => {
   const userInputButton = document.getElementById("submit");
   const userInput = document.getElementById("user-input");
+
   userInputButton.addEventListener("click", (e) => {
     e.preventDefault();
     if (!isCheckValidUserInput(userInput.value)) {
