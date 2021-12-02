93974a3fb4d1464c9d65ed6d22379a4d83dcb949
veggie-garden <devcseo@gmail.com>
2021-12-02 19:34:24 +0900

    refactor: change import method due to changes of constants.js

--- src/check_valid.js
+++ src/check_valid.js
@@ -1,12 +1,7 @@
-import {
-  MAX_LENGTH,
-  USER_INPUT_INCLUDE_ZERO,
-  USER_INPUT_NUMBER_DUPLICATED,
-  USER_INPUT_NUMBER_LENGTH_NOT_EXCEED,
-} from "./constants.js";
+import { NUMS, USER_INPUT_ERROR_MESSAGE } from "./constants.js";
 
 const isNotExceedCondition = (userInput) => {
-  return userInput.length !== MAX_LENGTH;
+  return userInput.length !== NUMS.MAX_LENGTH;
 };
 
 const isZeroInclude = (userInput) => {
@@ -14,20 +9,20 @@ const isZeroInclude = (userInput) => {
 };
 
 const isDuplicatedUserInput = (userInput) => {
-  return [...new Set(userInput)].length !== MAX_LENGTH;
+  return [...new Set(userInput)].length !== NUMS.MAX_LENGTH;
 };
 
 export default function isCheckValidUserInput(userInput) {
   if (isNotExceedCondition(userInput)) {
-    alert(USER_INPUT_NUMBER_LENGTH_NOT_EXCEED);
+    alert(USER_INPUT_ERROR_MESSAGE.LENGTH_NOT_EXCEED);
     return false;
   }
   if (isDuplicatedUserInput(userInput)) {
-    alert(USER_INPUT_NUMBER_DUPLICATED);
+    alert(USER_INPUT_ERROR_MESSAGE.DUPLICATED);
     return false;
   }
   if (isZeroInclude(userInput)) {
-    alert(USER_INPUT_INCLUDE_ZERO);
+    alert(USER_INPUT_ERROR_MESSAGE.INCLUDE_ZERO);
     return false;
   }
   return true;
