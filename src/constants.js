538c1efc0a5a7e1a7ff838a73ee9f042ec83f0dc
veggie-garden <devcseo@gmail.com>
2021-12-02 19:32:57 +0900

    refactor: change export method and add localStorage random number key const

--- src/constants.js
+++ src/constants.js
@@ -1,25 +1,28 @@
-const MIN_NUMBER = 1;
-const MAX_NUMBER = 9;
-const MAX_LENGTH = 3;
+const NUMS = {
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 9,
+  MAX_LENGTH: 3,
+};
 
-const USER_INPUT_INCLUDE_ZERO = "0은 사용할 수 없습니다.";
-const USER_INPUT_NUMBER_DUPLICATED = "중복되는 숫자 없이 입력해주세요.";
-const USER_INPUT_NUMBER_LENGTH_NOT_EXCEED = "세 자릿수 숫자를 입력해주세요.";
+const USER_INPUT_ERROR_MESSAGE = {
+  INCLUDE_ZERO: "0은 사용할 수 없습니다.",
+  DUPLICATED: "중복되는 숫자 없이 입력해주세요.",
+  LENGTH_NOT_EXCEED: "세 자릿수 숫자를 입력해주세요.",
+};
 
-const NO_CORRECT_ANSWER = "낫싱";
-const CORRECT_ANSWER =
-  "<p><span>🎉</span> <strong>정답을 맞추셨습니다!</strong> <span>🎉</span></p>";
-const ASK_RESTART =
-  "<p><span>게임을 새로 시작하겠습니까?</span> <button id='game-restart-button'>게임 재시작</button></p>";
+const RESULT_REPLY = {
+  NO_CORRECT_ANSWER: "낫싱",
+  CORRECT_ANSWER:
+    "<p><span>🎉</span> <strong>정답을 맞추셨습니다!</strong> <span>🎉</span></p>",
+  ASK_RESTART:
+    "<p><span>게임을 새로 시작하겠습니까?</span> <button id='game-restart-button'>게임 재시작</button></p>",
+};
+
+const LOCALSTORAGE_RANDOM_NUMBER_KEY = "computerInputNumber";
 
 export {
-  MIN_NUMBER,
-  MAX_NUMBER,
-  MAX_LENGTH,
-  USER_INPUT_INCLUDE_ZERO,
-  USER_INPUT_NUMBER_DUPLICATED,
-  USER_INPUT_NUMBER_LENGTH_NOT_EXCEED,
-  NO_CORRECT_ANSWER,
-  CORRECT_ANSWER,
-  ASK_RESTART,
+  NUMS,
+  USER_INPUT_ERROR_MESSAGE,
+  RESULT_REPLY,
+  LOCALSTORAGE_RANDOM_NUMBER_KEY,
 };
